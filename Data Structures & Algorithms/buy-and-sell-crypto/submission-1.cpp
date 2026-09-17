5b73bc9265fcdb3eb1747ce1b62150e1e1f52997
Smit Patel <patel.smit3131@gmail.com>
2026-09-17 16:27:45 -0400

    Add: buy-and-sell-crypto - submission-1

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-1.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int maxprof = 0;
+        for(int i=0;i<prices.size()-1;i++){
+            
+            for(int j = i+1;j<prices.size();j++){
+                int prof = prices[j] - prices[i];
+                maxprof = max(maxprof,prof);
+            }
+
+
+        }
+
+        return maxprof;
+    }
+};
